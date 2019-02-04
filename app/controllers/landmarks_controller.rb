3e64e2c1b43ad1cdcbeb2510fd365b9c9773aac3
Joey Cho <cjy12182000@naver.com>
2019-02-04 10:28:00 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,4 +11,22 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/show" 
   end
+  
+  get '/figures/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+  
+  patch '/landmarks/:id' do
+    
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    
+    @figure.save
+
+    flash[:message] = "Successfully updated landmark."
+    
+    redirect("/landmarks/#{@landmark.id}")
+  end
+  
 end
